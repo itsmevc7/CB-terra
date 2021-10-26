6c0ec19ea3c4fadff228a2b21bcc39be52bc8719
itsmevc7 <92775257+itsmevc7@users.noreply.github.com>
2021-10-26 11:11:14 +0530

    Create Providers.tf

---
+++ Providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id   = var.azure_subscription_id
+  tenant_id         = var.azure_subscription_tenant_id
+  client_id         = var.service_principal_appid
+  client_secret     = var.service_principal_password
+}
